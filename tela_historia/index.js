28b691864452f1bff52a2efa4d38f8dc342c3979
Camilla Ucci de Menezes <camillaucci.menezes@gmail.com>
2024-06-11 10:34:42 -0300

    Não sei

--- tela_historia/index.js
+++ tela_historia/index.js
@@ -1,4 +1,13 @@
-const comecar = document.getElementById('btn-comecar')
+const comecar = document.getElementById('btn-comecar');
+const tituloPagina = document.getElementById('titulo-a');
+const descExercicio = document.getElementById('desc-a');
+
+function personalizarPaginaA(desc, titulo) {
+    descExercicio.textContent = desc;
+    tituloEx.textContent = titulo;
+    tituloPagina.textContent = titulo;
+}
+
 comecar.addEventListener('click',()=>{
     window.location.href = "../tela_capitulos/index.html"
 })
