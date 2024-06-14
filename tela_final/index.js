966d0f2fbdc9042e9344f429f0215010628432ad
MariaMsschs <maria.msschs@gmail.com>
2024-06-14 09:25:25 -0300

    tela final

---
+++ tela_final/index.js
@@ -0,0 +1,6 @@
+const voltarButton = document.getElementById('voltar');
+const certificadoButton = document.getElementById('certificado');
+
+voltarButton.addEventListener('click', () => {
+    window.location.href = '../index.html' 
+});
